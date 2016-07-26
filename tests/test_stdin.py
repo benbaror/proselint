0547f24c914af864c223f1dd7bb0e2e7313f9d06
Ben Bar-Or <benbaror@gmail.com>
2016-07-26 02:33:30 -0400

    Added test for exit code

--- tests/test_stdin.py
+++ tests/test_stdin.py
@@ -5,8 +5,13 @@ from proselint.command_line import proselint, demo_file
 
 runner = CliRunner()
 demo = runner.invoke(proselint, ['--demo'])
+assert demo.exit_code == 1
 
 with open(demo_file) as f:
     stdin = runner.invoke(proselint, ['--stdin'], input=f.read())
 
+assert stdin.exit_code == 1
+    
 assert stdin.output == demo.output.replace(demo_file, 'stdin')
+
+
